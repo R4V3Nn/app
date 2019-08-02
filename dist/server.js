2119de2b72d165437c251b2304b60c669b969ff0
Artem Kaseka <artem.kaseka@synapse.com>
2019-08-02 10:48:09 +0300

    first commit

--- dist/server.js
+++ dist/server.js
@@ -5,11 +5,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Mixing it up on port ${PORT}`);
+	console.log(`Mixing it up on port ${PORT}`);
 });
