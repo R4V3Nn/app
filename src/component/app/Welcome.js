c4218e5c20b5fb51cf3023de960cd97320b2f2f1
Artem Kaseka <artem.kaseka@synapse.com>
2019-08-06 14:01:20 +0300

    fix: lint

--- src/component/app/Welcome.js
+++ src/component/app/Welcome.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Welcome = () => (
 	<div className="welcome">
-		<h1>Welcome to my Appp!</h1>
+		<h1>Welcome to my App!</h1>
 		<h3>Good to see you here</h3>
 	</div>
 );
