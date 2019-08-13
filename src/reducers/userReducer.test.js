14157cc395a98db232ed122c3bfcfd5a396afc3d
Artem Kaseka <artem.kaseka@gmail.com>
2019-08-13 11:28:38 +0300

    fix: app.scss

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -49,5 +49,86 @@ describe('userReducer', () => {
 
 		expect(userReducer(initialState, action)).toEqual(state);
 	});
+
+	it('check ITEM_TOGGLE case with one id', () => {
+		const action = {
+			type: types.ITEM_TOGGLE,
+			payload: 'id-1',
+		};
+		const updatedState = {
+			...initialState,
+			selectedIds: ['id-1'],
+		};
+
+		expect(userReducer(initialState, action)).toEqual(updatedState);
+	});
+
+	it('check ITEM_TOGGLE case with duplicated id', () => {
+		const action = {
+			type: types.ITEM_TOGGLE,
+			payload: 'id-1',
+		};
+		const state = {
+			...initialState,
+			selectedIds: ['id-1'],
+		};
+		const updatedState = {
+			...state,
+			selectedIds: [],
+		};
+
+		expect(userReducer(state, action)).toEqual(updatedState);
+	});
+
+	it('check ITEM_TOGGLE case with new id', () => {
+		const action = {
+			type: types.ITEM_TOGGLE,
+			payload: 'id-2',
+		};
+		const state = {
+			...initialState,
+			selectedIds: ['id-1'],
+		};
+		const updatedState = {
+			...state,
+			selectedIds: ['id-1', 'id-2'],
+		};
+
+		expect(userReducer(state, action)).toEqual(updatedState);
+	});
+
+	it('check SELECT_ALL case', () => {
+		const action = {
+			type: types.SELECT_ALL,
+			payload: [1, 2, 3],
+		};
+		const state = {
+			...initialState,
+			selectedIds: [],
+		};
+		const updatedState = {
+			...state,
+			selectedIds: [...action.payload],
+		};
+
+		expect(userReducer(state, action)).toEqual(updatedState);
+	});
+
+	it('check UPDATE_REPOS case', () => {
+		const action = {
+			type: types.UPDATE_REPOS,
+			payload: { name: 'app', id: 3 },
+		};
+		const state = {
+			...initialState,
+			userRepos: { name: 'app2', id: 4 },
+		};
+		const updatedState = {
+			...state,
+			userRepos: { name: 'app', id: 3 },
+		};
+
+		expect(userReducer(state, action)).toEqual(updatedState);
+	});
 });
 
