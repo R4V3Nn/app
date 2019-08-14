3154aa90f4b11dcb26ca1f66765217b50e6516fe
Artem Kaseka <artem.kaseka@gmail.com>
2019-08-14 13:28:37 +0300

    add: home link in header

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 import { HeaderBar } from '@talend/react-components';
 
 const Header = () => {
 	const brand = {
 		id: 'brand',
 		label: 'My App',
+		onClick: () => browserHistory.push('/'),
 	};
 	return (
 		<HeaderBar brand={brand} />
